f0bad17bbd343d9743e1638ad112f9005feedfbf
Bruno Lima <brunolimawp@gmail.com>
2026-08-11 14:51:50 -0300

    roc curve

--- 05_metrics.py
+++ 05_metrics.py
@@ -51,20 +51,42 @@ y = df_analise["feliz"]
 # %%
 
 from sklearn import tree
+from sklearn import naive_bayes
+from sklearn import linear_model
 
 arvore = tree.DecisionTreeClassifier(random_state=42, 
                                      min_samples_leaf=5)
 
 arvore.fit(X=X, y=y)
 
+naive = naive_bayes.GaussianNB()
+naive.fit(X=X, y=y)
+
+lin_reg = linear_model.LogisticRegression()
+lin_reg.fit(X=X, y=y)
+
 # %%
 
-arvore_predict = arvore.predict(X)
-arvore_predict
+df_predict = df_analise[["feliz"]].copy()
 
-df_predict = df_analise[["feliz"]]
+# ÁRVORE DE DECISÃO
+arvore_predict = arvore.predict(X)
+
 df_predict["predict_arvore"] = arvore_predict
-df_predict
+df_predict["proba_arvore"] = arvore.predict_proba(X)[:,1]     
+
+# NAIVE BAYES
+naive_predict = naive.predict(X)
+
+df_predict["predict_naive"] = naive_predict
+df_predict["proba_naive"] = naive.predict_proba(X)[:,1] 
+
+# REGRESSÃO LOGÍSTICA
+lin_reg_predict = lin_reg.predict(X)
+
+df_predict["predict_lin_reg"] = lin_reg_predict
+df_predict["proba_lin_reg"] = lin_reg.predict_proba(X)[:,1] 
+
 
 # %%
 # ACURÁCIA
@@ -72,3 +94,44 @@ df_predict
 
 # %%
 pd.crosstab(df_predict["feliz"], df_predict["predict_arvore"])
+# %%
+
+from sklearn import metrics
+
+acc_arvore = metrics.accuracy_score(df_predict["feliz"], df_predict["predict_arvore"])
+precision_arvore = metrics.precision_score(df_predict["feliz"], df_predict["predict_arvore"])
+recall_arvore = metrics.recall_score(df_predict["feliz"], df_predict["predict_arvore"])
+roc_arvore = metrics.roc_curve(df_predict["feliz"], df_predict["proba_arvore"])
+auc_arvore = metrics.roc_auc_score(df_predict["feliz"], df_predict["proba_arvore"])
+
+acc_naive = metrics.accuracy_score(df_predict["feliz"], df_predict["predict_naive"])
+precision_naive = metrics.precision_score(df_predict["feliz"], df_predict["predict_naive"])
+recall_naive = metrics.recall_score(df_predict["feliz"], df_predict["predict_naive"])
+roc_naive = metrics.roc_curve(df_predict["feliz"], df_predict["proba_naive"])
+auc_naive = metrics.roc_auc_score(df_predict["feliz"], df_predict["proba_naive"])
+
+acc_lin_reg = metrics.accuracy_score(df_predict["feliz"], df_predict["predict_lin_reg"])
+precision_lin_reg = metrics.precision_score(df_predict["feliz"], df_predict["predict_lin_reg"])
+recall_lin_reg = metrics.recall_score(df_predict["feliz"], df_predict["predict_lin_reg"])
+roc_lin_reg = metrics.roc_curve(df_predict["feliz"], df_predict["proba_lin_reg"])
+auc_lin_reg = metrics.roc_auc_score(df_predict["feliz"], df_predict["proba_lin_reg"])
+
+# %%
+import matplotlib.pyplot as plt
+
+# %%
+plt.plot(roc_arvore[0], roc_arvore[1], "o-")
+plt.plot(roc_naive[0], roc_naive[1], "o-")
+plt.plot(roc_lin_reg[0], roc_lin_reg[1], "o-")
+
+
+plt.grid(True)
+plt.title("ROC Curve")
+plt.xlabel("1 - Especificidade")
+plt.ylabel("Recall")
+
+plt.legend([f"Árvore: {auc_arvore:.2f}", f"Naive Bayes: {auc_naive:.2f}", f"Regressão Logística: {auc_lin_reg:.2f}"])
+
+plt.show()
+
+# %%
